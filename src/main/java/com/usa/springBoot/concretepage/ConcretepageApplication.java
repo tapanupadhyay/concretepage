142eb030624aecf6d40a2606f77d8790b54b935f
Tapan Upadhyay <tapan.upadhyay@gmail.com>
2018-05-22 22:03:01 -0500

    Initial commit

---
+++ src/main/java/com/usa/springBoot/concretepage/ConcretepageApplication.java
@@ -0,0 +1,12 @@
+package com.usa.springBoot.concretepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcretepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcretepageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/usa/springBoot/concretepage/ConcretepageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.usa.springBoot.concretepage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcretepageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
